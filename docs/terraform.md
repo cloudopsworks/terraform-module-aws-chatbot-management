5b6085848e2440a2b4eb86aea85e96750456f568
Cristian Beraha <cristian@cloudops.works>
2025-03-21 22:02:00 -0300

    Add documentation and resources for AWS Chatbot configuration
    Introduced detailed Terraform resources and variables to support Amazon Q Developer Chatbot configurations. Updated relevant documentation, including README, with setup instructions, makefile targets, and usage guidelines.

---
+++ docs/terraform.md
@@ -0,0 +1,45 @@
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.3 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | 5.92.0 |
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_tags"></a> [tags](#module\_tags) | cloudopsworks/tags/local | 1.0.9 |
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_chatbot_slack_channel_configuration.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/chatbot_slack_channel_configuration) | resource |
+| [aws_chatbot_teams_channel_configuration.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/chatbot_teams_channel_configuration) | resource |
+| [aws_iam_role.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [aws_iam_role_policy.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
+| [aws_iam_role_policy_attachment.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment) | resource |
+| [aws_chatbot_slack_workspace.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/chatbot_slack_workspace) | data source |
+| [aws_iam_policy_document.assume_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
+| [aws_iam_policy_document.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
+| [aws_region.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/region) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_configs"></a> [configs](#input\_configs) | Configuration object describing all the Amazon Q Chatbot configurations | `any` | `{}` | no |
+| <a name="input_extra_tags"></a> [extra\_tags](#input\_extra\_tags) | Extra tags to add to the resources | `map(string)` | `{}` | no |
+| <a name="input_is_hub"></a> [is\_hub](#input\_is\_hub) | Is this a hub or spoke configuration? | `bool` | `false` | no |
+| <a name="input_org"></a> [org](#input\_org) | Organization details | <pre>object({<br/>    organization_name = string<br/>    organization_unit = string<br/>    environment_type  = string<br/>    environment_name  = string<br/>  })</pre> | n/a | yes |
+| <a name="input_spoke_def"></a> [spoke\_def](#input\_spoke\_def) | Spoke ID Number, must be a 3 digit number | `string` | `"001"` | no |
+
+## Outputs
+
+No outputs.
--- main.tf
+++ main.tf
@@ -4,3 +4,94 @@
 #            Distributed Under Apache v2.0 License
 #
 
+data "aws_chatbot_slack_workspace" "this" {
+  for_each = {
+    for key, config in var.configs : key => config
+    if try(config.slack, null) != null
+  }
+  slack_team_name = each.value.slack.workspace_name
+}
+
+resource "aws_iam_role" "this" {
+  for_each = {
+    for key, config in var.configs : key => config
+  }
+  name               = format("%s-%s-chatbot-role", each.key, local.system_name)
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = [
+        "chatbot.amazonaws.com"
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid    = "CloudWatchGet"
+    effect = "Allow"
+    actions = [
+      "cloudwatch:Describe*",
+      "cloudwatch:Get*",
+      "cloudwatch:List*"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "this" {
+  for_each = {
+    for key, config in var.configs : key => config
+  }
+  role   = aws_iam_role.this[each.key].id
+  name   = "chatbot-policy"
+  policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  for_each = {
+    for key, config in var.configs : key => config
+  }
+  role       = aws_iam_role.this[each.key].name
+  policy_arn = "arn:aws:iam::aws:policy/AWSResourceExplorerReadOnlyAccess"
+}
+
+resource "aws_chatbot_slack_channel_configuration" "this" {
+  for_each = {
+    for key, config in var.configs : key => config
+    if try(config.slack, null) != null
+  }
+  configuration_name          = try(each.value.name, "") != "" ? each.value.name : format("%s-%s-slack-chatbot", try(each.value.name_prefix, each.key), local.system_name_short)
+  iam_role_arn                = aws_iam_role.this[each.key].arn
+  guardrail_policy_arns       = try(each.value.guardrail_policy_arns, null)
+  slack_channel_id            = each.value.slack.channel_id
+  slack_team_id               = data.aws_chatbot_slack_workspace.this[each.key].slack_team_id
+  logging_level               = try(each.value.logging_level, null)
+  sns_topic_arns              = try(each.value.sns_topic_arns, null)
+  user_authorization_required = try(each.value.user_authorization_required, null)
+  tags                        = local.all_tags
+}
+
+resource "aws_chatbot_teams_channel_configuration" "this" {
+  for_each = {
+    for key, config in var.configs : key => config
+    if try(config.teams, null) != null
+  }
+  configuration_name = try(each.value.name, "") != "" ? each.value.name : format("%s-%s-teams-chatbot", try(each.value.name_prefix, each.key), local.system_name_short)
+  channel_id         = each.value.teams.channel_id
+  iam_role_arn       = aws_iam_role.this[each.key].arn
+  team_id            = each.value.teams.team_id
+  tenant_id          = each.value.teams.tenant_id
+  tags               = local.all_tags
+}
+
+
